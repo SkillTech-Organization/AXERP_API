72cff71578c004ec7fd4f6cabce770c3c6f5f5d5
jeryxdeveloper <jeryx@freemail.hu>
2024-08-13 19:06:32 +0200

    mergint Create, Update into CreateOrUpdate to decrease code duplication

--- AXERP.API.Business/Commands/InsertTransactionsCommand.cs
+++ AXERP.API.Business/Commands/InsertTransactionsCommand.cs
@@ -110,7 +110,7 @@ namespace AXERP.API.Business.Commands
                      */
                     _logger.LogInformation("Creating transactions...");
 
-                    Create(uow, newSheetRows);
+                    CreateOrUpdate(uow, newSheetRows, true);
 
                     _logger.LogInformation("Refreshing data for processing. Transactions, entities...");
                     RefreshBusinessDataCache(uow);
@@ -120,7 +120,7 @@ namespace AXERP.API.Business.Commands
                      */
                     _logger.LogInformation("Updating transactions...");
 
-                    Update(uow, updatedSheetRows);
+                    CreateOrUpdate(uow, updatedSheetRows, false);
 
                     uow.CommitTransaction();
 
@@ -166,7 +166,7 @@ namespace AXERP.API.Business.Commands
             uow.Save("delete_done");
         }
 
-        private void Update(IUnitOfWork uow, IEnumerable<Delivery> sheetRows)
+        private void CreateOrUpdate(IUnitOfWork uow, IEnumerable<Delivery> sheetRows, bool create)
         {
             if (!sheetRows.Any())
             {
@@ -332,9 +332,16 @@ namespace AXERP.API.Business.Commands
                 transactionDtos.Add(transaction);
             }
 
-            _logger.LogInformation("Updating {name} rows. Count: {count}", nameof(Transaction), transactionDtos.Count);
-            uow.TransactionRepository.Update(transactionDtos);
-            // uow.GenericRepository.BulkCopy<Transaction>(transactionDtos);
+            if (create)
+            {
+                _logger.LogInformation("Inserting new {name} rows. Count: {count}", nameof(Transaction), transactionDtos.Count);
+                uow.GenericRepository.BulkCopy<Transaction>(transactionDtos);
+            }
+            else
+            {
+                _logger.LogInformation("Updating {name} rows. Count: {count}", nameof(Transaction), transactionDtos.Count);
+                uow.TransactionRepository.Update(transactionDtos);
+            }
 
             _logger.LogInformation("Inserting new {name} rows. Count: {count}", nameof(CustomerToDelivery), ctdNew.Count);
             // uow.CustomerToDeliveryRepository.Add(ctdNew);
@@ -353,195 +360,6 @@ namespace AXERP.API.Business.Commands
             //uow.GenericRepository.BulkCopy<TruckCompanyToDelivery>(ttdUpdate);
 
             uow.Save("update_done");
-        }
-
-        private void Create(IUnitOfWork uow, IEnumerable<Delivery> sheetRows)
-        {
-            if (!sheetRows.Any())
-            {
-                return;
-            }
-
-            var transactionDtos = new List<Transaction>();
-            var ctdNew = new List<CustomerToDelivery>();
-            var ttdNew = new List<TruckCompanyToDelivery>();
-            var ctdUpdate = new List<CustomerToDelivery>();
-            var ttdUpdate = new List<TruckCompanyToDelivery>();
-
-            foreach (var sheetRow in sheetRows)
-            {
-                var transaction = _mapper.Map<Transaction>(sheetRow);
-
-                transaction.ID = sheetRow.DeliveryID;
-
-                transaction.StatusID = sheetRow.Status;
-                transaction.SalesStatusID = sheetRow.SalesStatus;
-
-                transaction.AXERPHash = sheetRow.AXERPHash;
-
-                var newTerminal = Interfaces.FirstOrDefault(x => x.Name == sheetRow.Terminal);
-                if (newTerminal == null && !string.IsNullOrWhiteSpace(sheetRow.Terminal) && !Interfaces.Any(x => x.Name == sheetRow.Terminal))
-                {
-                    newTerminal = uow.InterfaceRepository.Add(new Interface { Name = sheetRow.Terminal });
-                    Interfaces.Add(newTerminal);
-                }
-
-                var newDeliveryPoint = Interfaces.FirstOrDefault(x => x.Name == sheetRow.DeliveryPoint);
-                if (newDeliveryPoint == null && !string.IsNullOrWhiteSpace(sheetRow.DeliveryPoint) && !Interfaces.Any(x => x.Name == sheetRow.DeliveryPoint))
-                {
-                    newDeliveryPoint = uow.InterfaceRepository.Add(new Interface { Name = sheetRow.DeliveryPoint });
-                    Interfaces.Add(newDeliveryPoint);
-                }
-
-                var newSpecificPoint = Interfaces.FirstOrDefault(x => x.Name == sheetRow.SpecificDeliveryPoint);
-                if (newSpecificPoint == null && !string.IsNullOrWhiteSpace(sheetRow.SpecificDeliveryPoint) && !Interfaces.Any(x => x.Name == sheetRow.SpecificDeliveryPoint))
-                {
-                    newSpecificPoint = uow.InterfaceRepository.Add(new Interface { Name = sheetRow.SpecificDeliveryPoint });
-                    Interfaces.Add(newSpecificPoint);
-                }
-
-                var newRef3 = Documents.FirstOrDefault(x => x.Name == sheetRow.Reference3);
-                if (newRef3 == null && !string.IsNullOrWhiteSpace(sheetRow.Reference3) && !Documents.Any(x => x.Name == sheetRow.Reference3))
-                {
-                    newRef3 = uow.DocumentRepository.Add(new Document { Name = sheetRow.Reference3 });
-                    Documents.Add(newRef3);
-                    transaction.BlFileID = newRef3.ID;
-                }
-
-                var newRef2 = Documents.FirstOrDefault(x => x.Name == sheetRow.Reference2);
-                if (newRef2 == null && !string.IsNullOrWhiteSpace(sheetRow.Reference2) && !Documents.Any(x => x.Name == sheetRow.Reference2))
-                {
-                    newRef2 = uow.DocumentRepository.Add(new Document { Name = sheetRow.Reference2 });
-                    Documents.Add(newRef2);
-                    transaction.BlFileID = newRef2.ID;
-                }
-
-                var newRef = Documents.FirstOrDefault(x => x.Name == sheetRow.Reference);
-                if (newRef == null && !string.IsNullOrWhiteSpace(sheetRow.Reference) && !Documents.Any(x => x.Name == sheetRow.Reference))
-                {
-                    newRef = uow.DocumentRepository.Add(new Document { Name = sheetRow.Reference });
-                    Documents.Add(newRef);
-                    transaction.BlFileID = newRef.ID;
-                }
-
-                var newTransporter = Entities.FirstOrDefault(x => x.Name == sheetRow.Transporter);
-                if (newTransporter == null && !string.IsNullOrWhiteSpace(sheetRow.Transporter) && !Entities.Any(x => x.Name == sheetRow.Transporter))
-                {
-                    newTransporter = uow.EntityRepository.Add(new Entity { Name = sheetRow.Transporter });
-                    Entities.Add(newTransporter);
-                }
-
-                var isTruckCompanyNew = false;
-                var sheetTruckCompany = TruckCompanies.FirstOrDefault(x => x.Name == sheetRow.TruckCompany);
-                if (sheetTruckCompany == null && !string.IsNullOrWhiteSpace(sheetRow.TruckCompany) && !TruckCompanies.Any(x => x.Name == sheetRow.TruckCompany))
-                {
-                    sheetTruckCompany = uow.TruckCompanyRepository.Add(new Entity { Name = sheetRow.TruckCompany });
-                    TruckCompanies.Add(sheetTruckCompany);
-                    isTruckCompanyNew = true;
-                }
-
-                var isCustomerNew = false;
-                var sheetCustomer = Entities.FirstOrDefault(x => x.Name == sheetRow.Customer);
-                if (sheetCustomer == null && !string.IsNullOrWhiteSpace(sheetRow.Customer) && !Entities.Any(x => x.Name == sheetRow.Customer))
-                {
-                    sheetCustomer = uow.EntityRepository.Add(new Entity { Name = sheetRow.Customer });
-                    Entities.Add(sheetCustomer);
-                    isCustomerNew = true;
-                }
-
-                transaction.TerminalID = newTerminal?.ID;
-
-                transaction.DeliveryPointID = newDeliveryPoint?.ID;
-                transaction.SpecificDeliveryPointID = newSpecificPoint?.ID;
-
-                transaction.TransporterID = newTransporter?.ID;
-
-                if (sheetCustomer != null)
-                {
-                    if (!isCustomerNew)
-                    {
-                        var u = CustomerToDeliveries.FirstOrDefault(x => x.DeliveryID == sheetRow.DeliveryID);
-                        if (u != null)
-                        {
-                            u.Comment = sheetRow.CustomerNote;
-                            ctdUpdate.Add(u);
-                        }
-                        else
-                        {
-                            ctdNew.Add(new CustomerToDelivery
-                            {
-                                DeliveryID = transaction.ID,
-                                CustomerID = sheetCustomer.ID,
-                                Comment = sheetRow.CustomerNote
-                            });
-                        }
-                    }
-                    else
-                    {
-                        ctdNew.Add(new CustomerToDelivery
-                        {
-                            DeliveryID = transaction.ID,
-                            CustomerID = sheetCustomer.ID,
-                            Comment = sheetRow.CustomerNote
-                        });
-                    }
-                }
-
-                if (sheetTruckCompany != null)
-                {
-                    if (!isTruckCompanyNew)
-                    {
-                        var u = TruckCompanyToDeliveries.FirstOrDefault(x => x.DeliveryID == sheetRow.DeliveryID);
-                        if (u != null)
-                        {
-                            u.Comment = sheetRow.TruckLoadingCompanyComment;
-                            ttdUpdate.Add(u);
-                        }
-                        else
-                        {
-                            ttdNew.Add(new TruckCompanyToDelivery
-                            {
-                                DeliveryID = transaction.ID,
-                                TruckCompanyID = sheetTruckCompany.ID,
-                                Comment = sheetRow.TruckLoadingCompanyComment
-                            });
-                        }
-                    }
-                    else
-                    {
-                        ttdNew.Add(new TruckCompanyToDelivery
-                        {
-                            DeliveryID = transaction.ID,
-                            TruckCompanyID = sheetTruckCompany.ID,
-                            Comment = sheetRow.TruckLoadingCompanyComment
-                        });
-                    }
-                }
-
-                transactionDtos.Add(transaction);
-            }
-
-            _logger.LogInformation("Inserting new {name} rows. Count: {count}", nameof(Transaction), transactionDtos.Count);
-            //uow.TransactionRepository.Add(transactionDtos, true);
-            uow.GenericRepository.BulkCopy<Transaction>(transactionDtos);
-
-            _logger.LogInformation("Inserting new {name} rows. Count: {count}", nameof(CustomerToDelivery), ctdNew.Count);
-            //uow.CustomerToDeliveryRepository.Add(ctdNew);
-            uow.GenericRepository.BulkCopy<CustomerToDelivery>(ctdNew);
-
-            _logger.LogInformation("Inserting new {name} rows. Count: {count}", nameof(TruckCompanyToDelivery), ttdNew.Count);
-            //uow.TruckCompanyToDeliveryRepository.Add(ttdNew);
-            uow.GenericRepository.BulkCopy<TruckCompanyToDelivery>(ttdNew);
-
-            _logger.LogInformation("Updating {name} rows. Count: {count}", nameof(CustomerToDelivery), ctdUpdate.Count);
-            uow.CustomerToDeliveryRepository.Update(ctdUpdate);
-            //uow.GenericRepository.BulkCopy<CustomerToDelivery>(ctdUpdate);
-
-            _logger.LogInformation("Updating {name} rows. Count: {count}", nameof(TruckCompanyToDelivery), ttdUpdate.Count);
-            uow.TruckCompanyToDeliveryRepository.Update(ttdUpdate);
-            //uow.GenericRepository.BulkCopy<TruckCompanyToDelivery>(ttdUpdate);
-
-            uow.Save("create_done");
         }
     }
 }
