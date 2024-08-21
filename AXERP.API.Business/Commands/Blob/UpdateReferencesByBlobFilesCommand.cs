b146b25002378e84bf25b10833ed99a3777425c2
kjeremi <kdrisaac42@gmail.com>
2024-08-21 22:37:34 +0200

    blob processing stats fixed

--- AXERP.API.Business/Commands/Blob/UpdateReferencesByBlobFilesCommand.cs
+++ AXERP.API.Business/Commands/Blob/UpdateReferencesByBlobFilesCommand.cs
@@ -32,7 +32,7 @@ namespace AXERP.API.Business.Commands
             {
                 _logger.LogInformation("Success! Process BL files statistics: {0}", Newtonsoft.Json.JsonConvert.SerializeObject(result));
             }
-            else if (result.Errors.Count >= 0 && result.Processed.Count >= 0)
+            else if (result.Errors.Count > 0 && result.Processed.Count > 0)
             {
                 _logger.LogWarning("Not all files could be processed! Process BL files statistics: {0}", Newtonsoft.Json.JsonConvert.SerializeObject(result));
             }
